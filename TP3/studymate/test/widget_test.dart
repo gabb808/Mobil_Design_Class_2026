e43476988fc6386ae22068f20025427a65552c14
Gabriel <hayuyi9@gmail.com>
2026-03-23 14:31:28 +0100

    Remove TP3 folder and clean repository - keep only NeighborDrop

--- TP3/studymate/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:studymate/main.dart';
-
-void main() {
-  testWidgets('NeighborDrop smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const NeighborDropApp());
-  });
-}
